793f11d3f9f3c17669d5582c93190825e035a7c1
jsseidel <jsseidel@fastmail.com>
2018-07-10 18:28:49 -0400

    Added snap packaging

---
+++ snap/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export JAVA_HOME=$SNAP/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$JAVA_HOME/jre/bin:$PATH
+
+# Not good, needed for fontconfig
+export XDG_DATA_HOME=$SNAP/usr/share
+# Font Config
+export FONTCONFIG_PATH=$SNAP/etc/fonts/config.d
+export FONTCONFIG_FILE=$SNAP/etc/fonts/fonts.conf
+
+#export PULSE_RUNTIME_PATH=/var/run/pulse
+#export PULSE_SYSTEM=1
+
+ARCH=x86_64-linux-gnu
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SNAP/usr/lib/$ARCH/pulseaudio
+
+java -jar $SNAP/jgalaxian-1.0-SNAPSHOT.jar
